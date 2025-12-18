32f3f95ed0bfc6b45671709de1735048961514f9
alfonsojaureguibehere <alfonso.jaureguibehere@gmail.com>
2025-12-18 16:38:14 -0300

    task 1 (#2)
    * i do a program to calculate the funtions of a the differents equations (polynomial, factored, canonical) about the numbers given by the user
    
    * i put all in eanglish
    the name of the variable
    and the result if root it´s 0

--- quadplot/quadplot.py
+++ quadplot/quadplot.py
@@ -1,2 +1,31 @@
-print("Hello, world!")
+import math
 
+#values
+a = int(input ("value of a: "))
+b = int(input ("value of b: "))
+c = int(input ("value of c: "))
+
+#main equation
+print (f"the main equation is: {a}.x^2 + {b}.x + {c}")
+
+#roots
+root = b ** 2 - 4*a*c
+xv = -b / 2*a
+
+if root < 0:
+    print ("hasn´t roots (it hasn´t factored form)")
+
+else: 
+    if root == 0:
+        print (f"the factored form is: {(a)}.(x-{(xv)}).(x-{(xv)})")
+    else:
+        a = (-b + math.sqrt(root)) /2*a
+        b = (-b - math.sqrt(root)) /2*a
+
+        print (f"the factored form is: {a}.(x-{(a)}).(x-{(b)})")
+
+#vertex
+yv = a * xv ** 2 + b*xv + c
+
+#result
+print (f"the canonical form is: {a}.(x-{(xv)})^2-{(yv)}")
