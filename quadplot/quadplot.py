2c1df47c90ecfddfb2feaaaaba0a626a86d7a173
DESKTOP-IS87K92\s7 <alfonso.jaureguibehere@gmail.com>
2025-12-18 15:58:05 -0300

    i put all in eanglish the name of the variable and the result if root it´s 0

--- quadplot/quadplot.py
+++ quadplot/quadplot.py
@@ -1,32 +1,31 @@
 import math
 
 #values
-a = int(input ("valor de a: "))
-b = int(input ("valor de b: "))
-c = int(input ("valor de c: "))
+a = int(input ("value of a: "))
+b = int(input ("value of b: "))
+c = int(input ("value of c: "))
 
 #main equation
-print (f"la ecuacion principal es: {a}.x^2 + {b}.x + {c}")
+print (f"the main equation is: {a}.x^2 + {b}.x + {c}")
 
 #roots
 root = b ** 2 - 4*a*c
-rot = -b / 2*a
+xv = -b / 2*a
 
 if root < 0:
-    print ("no hay raices (no hay forma factorizada)")
+    print ("hasn´t roots (it hasn´t factored form)")
 
 else: 
     if root == 0:
-        print ("la raiz es:" + rot + "(no hay forma factorizada)")
+        print (f"the factored form is: {(a)}.(x-{(xv)}).(x-{(xv)})")
     else:
         a = (-b + math.sqrt(root)) /2*a
         b = (-b - math.sqrt(root)) /2*a
 
-        print (f"la forma factorizada es: {a}.(x-{(a)}).(x-{(b)})")
+        print (f"the factored form is: {a}.(x-{(a)}).(x-{(b)})")
 
 #vertex
-xv = -b /2*a
 yv = a * xv ** 2 + b*xv + c
 
 #result
-print (f"la forma canonica es: {a}.(x-{(xv)})^2-{(yv)}")
+print (f"the canonical form is: {a}.(x-{(xv)})^2-{(yv)}")
